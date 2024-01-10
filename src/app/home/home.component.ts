8460b7507868cc67febb9de73587bd4307dcb0be
Khanh Chi Tran <khanhchi@Khanhs-MBP.fritz.box>
2024-01-10 21:56:23 +0100

    declare Input property and bind housingLocation property

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { LocationComponent } from '../location/location.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HousingLocation } from '../housing-location';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LocationComponent, MatSlideToggleModule],
+  imports: [LocationComponent, NgFor],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  title = 'home';
+  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
+  housingLocationList: HousingLocation[] = [
+    {
+      id: 9999,
+      name: 'Test Home',
+      city: 'Test city',
+      state: 'ST',
+      photo: `${this.baseUrl}/example-house.jpg`,
+      availableUnits: 99,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 9992,
+      name: 'Test Home',
+      city: 'Test city',
+      state: 'ST',
+      photo: `${this.baseUrl}/example-house.jpg`,
+      availableUnits: 100,
+      wifi: true,
+      laundry: false,
+    },
+  ];
 }
--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-location',
   standalone: true,
   imports: [],
   templateUrl: './location.component.html',
-  styleUrl: './location.component.scss'
+  styleUrl: './location.component.scss',
 })
 export class LocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
