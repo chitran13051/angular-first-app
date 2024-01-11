3632345b834a744b9be5a9bc4cd800e2667b9387
Chi Tran <khanhchi.tran13051@gmail.com>
2024-01-11 21:53:03 +0100

    create Angular service

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
 @Component({
   selector: 'app-details',
   standalone: true,
@@ -10,8 +12,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
+  housingService: HousingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationId);
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LocationComponent } from '../location/location.component';
 import { HousingLocation } from '../housing-location';
 import { NgFor } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
@@ -12,77 +13,9 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
-  housingLocationList: HousingLocation[] = [
-    {
-      id: 9999,
-      name: 'Test Home',
-      city: 'Test city',
-      state: 'ST',
-      photo: `${this.baseUrl}/example-house.jpg`,
-      availableUnits: 99,
-      wifi: true,
-      laundry: false,
-    },
-    {
-      id: 9992,
-      name: 'Test Home',
-      city: 'Test city',
-      state: 'ST',
-      photo: `${this.baseUrl}/example-house.jpg`,
-      availableUnits: 100,
-      wifi: true,
-      laundry: false,
-    },
-    {
-      id: 9992,
-      name: 'Test Home',
-      city: 'Test city',
-      state: 'ST',
-      photo: `${this.baseUrl}/example-house.jpg`,
-      availableUnits: 100,
-      wifi: true,
-      laundry: false,
-    },
-    {
-      id: 9992,
-      name: 'Test Home',
-      city: 'Test city',
-      state: 'ST',
-      photo: `${this.baseUrl}/example-house.jpg`,
-      availableUnits: 100,
-      wifi: true,
-      laundry: false,
-    },
-    {
-      id: 9992,
-      name: 'Test Home',
-      city: 'Test city',
-      state: 'ST',
-      photo: `${this.baseUrl}/example-house.jpg`,
-      availableUnits: 100,
-      wifi: true,
-      laundry: false,
-    },
-    {
-      id: 9992,
-      name: 'Test Home',
-      city: 'Test city',
-      state: 'ST',
-      photo: `${this.baseUrl}/example-house.jpg`,
-      availableUnits: 100,
-      wifi: true,
-      laundry: false,
-    },
-    {
-      id: 9992,
-      name: 'Test Home',
-      city: 'Test city',
-      state: 'ST',
-      photo: `${this.baseUrl}/example-house.jpg`,
-      availableUnits: 100,
-      wifi: true,
-      laundry: false,
-    },
-  ];
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
