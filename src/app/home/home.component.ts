a1d7a0060610ff86f05db458ec752de299498d23
Chi Tran <khanhchi.tran13051@gmail.com>
2024-01-15 20:32:21 +0100

    render housing cards using Angular Material mat-grid-list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,11 +4,12 @@ import { HousingLocation } from '../housing-location';
 import { NgFor } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HousingService } from '../housing.service';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LocationComponent, NgFor, RouterModule],
+  imports: [LocationComponent, NgFor, RouterModule, MatGridListModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -2,10 +2,13 @@ import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housing-location';
 import { RouterModule } from '@angular/router';
 import { DetailsComponent } from '../details/details.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 @Component({
   selector: 'app-location',
   standalone: true,
-  imports: [RouterModule, DetailsComponent],
+  imports: [RouterModule, DetailsComponent, MatCardModule, MatGridListModule],
   templateUrl: './location.component.html',
   styleUrl: './location.component.scss',
 })
